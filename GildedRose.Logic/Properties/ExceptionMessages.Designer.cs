cb05108b3c21e05336de1c49f63c729f505b0978
SpeedFreak259 <andy@objectreality.co.uk>
2016-04-04 00:51:48 +0100

    Moved ExceptionMessages.resx to Properties

--- GildedRose.Logic/Properties/ExceptionMessages.Designer.cs
+++ GildedRose.Logic/Properties/ExceptionMessages.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GildedRose.Logic {
+namespace GildedRose.Logic.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GildedRose.Logic {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GildedRose.Logic.ExceptionMessages", typeof(ExceptionMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GildedRose.Logic.Properties.ExceptionMessages", typeof(ExceptionMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
