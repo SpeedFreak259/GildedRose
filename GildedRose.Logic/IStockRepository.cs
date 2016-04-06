b3ba42f6a74441794ec09b62aa149e31b6771588
SpeedFreak259 <andy@objectreality.co.uk>
2016-04-06 23:20:19 +0100

    Added simple repository interface to load and save the store stock.

---
+++ GildedRose.Logic/IStockRepository.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="IStockRepository.cs" company="Andy Baker">
+// See MIT-LICENSE.txt
+// </copyright>
+
+namespace GildedRose.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using GildedRose.Model;
+
+    /// <summary>
+    /// Defines the repository interface for the stock inventory.
+    /// </summary>
+    public interface IStockRepository
+    {
+        /// <summary>
+        /// Loads the stock list.
+        /// </summary>
+        /// <returns>Returns a <see cref="IList{T}"/> of <see cref="StockItem"/>.</returns>
+        Task<IList<StockItem>> LoadStockList();
+
+        /// <summary>
+        /// Saves the stock list.
+        /// </summary>
+        /// <param name="stockList">The stock items to persist.</param>
+        /// <returns>A task of bool.</returns>
+        Task<bool> SaveStockList(IEnumerable<StockItem> stockList);
+    }
+}
